a53a18ab4c114c42e914e14ac0c87c8f8917625e
Lawrence Jay Gabionza <gabionzalawrencejay@gmail.com>
2026-02-23 05:23:09 +0800

    Delete php directory

--- php/fix_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db_connect.php';
-$sql = "ALTER TABLE products ADD COLUMN is_available TINYINT(1) DEFAULT 1";
-if ($conn->query($sql)) {
-    echo "Column 'is_available' added successfully!";
-} else {
-    echo "Error adding column: " . $conn->error;
-}
-?>
